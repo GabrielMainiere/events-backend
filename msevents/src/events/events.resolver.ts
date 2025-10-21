3c17029bbb800d5fe8a56ab02b99885f7a86192c
Gabriel <gsmainiere1901@gmail.com>
2025-10-21 17:38:10 -0300

    feat: initial configuration

---
+++ msevents/src/events/events.resolver.ts
@@ -0,0 +1,20 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { EventsService } from './events.service';
+import { Event } from './entities/event.entity';
+import { CreateEventInput } from './dto/create-event.input';
+
+@Resolver(() => Event)
+export class EventsResolver {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Mutation(() => Event)
+  createEvent(@Args('createEventInput') createEventInput: CreateEventInput) {
+    return this.eventsService.create(createEventInput);
+  }
+
+  @Query(() => [Event], { name: 'events' })
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+}
