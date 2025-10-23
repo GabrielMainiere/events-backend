d058d87f41c0a56a6598750c56ef50f31a1d16d9
Gabriel <gsmainiere1901@gmail.com>
2025-10-23 00:09:07 -0300

    initial configuration of msevents

--- msevents/src/events/events.service.ts
+++ msevents/src/events/events.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventInput } from './dto/create-event.input';
+import { UpdateEventInput } from './dto/update-event.input';
 
 @Injectable()
 export class EventsService {
@@ -9,5 +10,17 @@ export class EventsService {
 
   findAll() {
     return `This action returns all events`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} event`;
+  }
+
+  update(id: number, updateEventInput: UpdateEventInput) {
+    return `This action updates a #${id} event`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} event`;
   }
 }
