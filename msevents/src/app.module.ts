c438de1ba305e093cabf242fcec50ad27d35d819
Gabriel Souza Mainiere <132918507+GabrielMainiere@users.noreply.github.com>
2025-10-25 13:31:57 -0300

    Feature/msevents (#2)
    * feat: implementing schemas.prisma
    
    * feat: Adding singleton on PrismaClient
    
    * feat: Adding dtos to events
    
    * feat: implementing builder pattern to events
    
    * feat: creating classes to event + implement Factory pattern
    
    * feat: creating create and findAll events
    
    * refactor: updating database
    
    * refactor: refactoring builder pattern
    
    * refactor: adjusting create event

--- msevents/src/app.module.ts
+++ msevents/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { EventsModule } from './events/events.module';
+import { join } from 'path';
 
 @Module({
-  imports: [EventsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    EventsModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
--- msevents/src/events/entities/event.entity.ts
+++ msevents/src/events/entities/event.entity.ts
@@ -1,7 +1,60 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { EventStatus } from 'generated/prisma';
+import { EventType } from 'generated/prisma';
 
 @ObjectType()
 export class Event {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  id: string;
+
+  @Field()
+  title: string;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Field()
+  startAt: Date;
+
+  @Field()
+  endAt: Date;
+
+  @Field(() => Int, { nullable: true })
+  price?: number; //cents
+
+  @Field({ nullable: true })
+  saleStartAt?: Date;
+
+  @Field({ nullable: true })
+  saleEndAt?: Date;
+
+  @Field()
+  addressStreet: string;
+
+  @Field({ nullable: true })
+  addressNumber?: string;
+
+  @Field()
+  addressCity: string;
+
+  @Field()
+  addressState: string;
+
+  @Field()
+  addressZipcode: string;
+
+  @Field()
+  addressCountry: string;
+
+  @Field(() => EventStatus)
+  status: EventStatus;
+
+  @Field(() => EventType)
+  eventType: EventType;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
 }
