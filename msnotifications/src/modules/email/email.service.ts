f00c8f27749f8a9973d2d9b77912e8630fa47750
Rafael Belmont <160741467+BelmontRafael@users.noreply.github.com>
2025-10-26 20:53:59 -0300

    Feature/msnotifications (#7)
    * creating msnotifications project
    
    * creating database
    
    * setting prisma module and main notification services
    
    * creating proto contract and setting main for grpc
    
    * creating notification implementations to enqueue notifications + resolving inconsistencies
    
    * organizing pastes and files
    
    * setup email service with nodemailer and gmail transport
    
    * implementing a  queue worker to process and send notifications (added Cron job to poll 'PENDING' logs, render templates with handlebars, and send emails via emailService)
    
    * implementing Strategy Pattern for notification channels (email/sms)
    
    * implementing Factory Pattern to select channel strategy
    
    * implementing Decorator Pattern for logs

---
+++ msnotifications/src/modules/email/email.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+
+//NestJS already utilizes singleton pattern for services.
+
+@Injectable()
+export class EmailService {
+  private transport;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: this.configService.get<string>('GMAIL_USER'),
+        pass: this.configService.get<string>('GMAIL_PASSWORD'),
+      },
+    });
+  }
+
+  async sendMail(to: string, subject: string, body: string) {
+    await this.transport.sendMail({
+      from: `"Events Project" <${this.configService.get<string>('GMAIL_USER')}>`,
+      to: to,
+      subject: subject,
+      html: body,
+    });
+  }
+}
