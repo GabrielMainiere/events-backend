e8d88b7af03a75784d7b6c0bc14bd2f763d3492c
Gabriel <gsmainiere1901@gmail.com>
2025-11-01 20:17:18 -0300

    fix: adjusting environment variables

--- mseventsregistration/src/core/environment.ts
+++ mseventsregistration/src/core/environment.ts
@@ -4,4 +4,5 @@ export const environment = {
   databaseUrl: process.env.DATABASE_URL || 'postgresql://user:pass@localhost:5432/dbname',
   graphqlPort: Number(process.env.GRAPHQL_PORT ?? '3001'),
   grpcPort: Number(process.env.GRPC_PORT ?? '50051'),
+  grpcPortMsusers: Number(process.env.GRPC_PORT_MSUSERS ?? '50053'),
 };
