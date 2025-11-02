4bacbda45281fe8cf82f08ee840b10d4d383fd75
Luiz Fernando <74717128+LuizReis-dev@users.noreply.github.com>
2025-11-02 16:35:05 -0300

    Feature/dockerizing events applications (#29)
    * dockerizing msevents
    
    * dockerizing mseventsregistration

--- msevents/src/core/environment.ts
+++ msevents/src/core/environment.ts
@@ -3,5 +3,5 @@ import 'dotenv/config';
 export const environment = {
   databaseUrl: process.env.DATABASE_URL || 'postgresql://user:pass@localhost:5432/dbname',
   graphqlPort: Number(process.env.GRAPHQL_PORT ?? '3000'),
-  grpcPortMseventsregistrations: Number(process.env.GRPC_PORT_MSEVENTSREGISTRATION ?? '50051'),
+  grpcMseventsregistrations: process.env.GRPC_MSEVENTSREGISTRATION ?? 'http://localhost:50051',
 };
