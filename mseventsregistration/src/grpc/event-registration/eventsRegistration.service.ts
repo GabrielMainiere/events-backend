a127fb4d687af059432a39969b116f6cdb29a3f6
Gabriel <gsmainiere1901@gmail.com>
2025-11-03 21:16:34 -0300

    feat: implementing service grpc getRegistration + mapper

--- mseventsregistration/src/grpc/event-registration/eventsRegistration.service.ts
+++ mseventsregistration/src/grpc/event-registration/eventsRegistration.service.ts
@@ -3,8 +3,8 @@ import { IEventNotificationResponse } from "./interfaces/IEventRegistrationRespo
 import { IEventRegistrationService } from "./interfaces/IEventRegistrationService";
 import { EventsRegistrationRepository } from "./eventsRegistration.repository";
 import { Injectable } from "@nestjs/common";
-import { IGetEventByIdRequest } from "./interfaces/IGetEventByIdRequest";
-import { IGetEventByIdResponse } from "./interfaces/IGetEventByIdResponse";
+import { IGetRegistrationRequest } from "./interfaces/IGetRegistrationRequest";
+import { IGetRegistrationResponse } from "./interfaces/IGetRegistrationResponse";
 import { IPaymentUpdateRequest } from "./interfaces/IPaymentUpdateRequest";
 import { IPaymentUpdateResponse } from "./interfaces/IPaymentUpdateResponse";
 import { RegistrationStatus } from "@prisma/client";
@@ -26,37 +26,58 @@ export class EventsRegistrationService implements IEventRegistrationService {
         return this.handleUpsert(data, 'CANCELLED');
     }
 
-    async getEventById(data: IGetEventByIdRequest): Promise<IGetEventByIdResponse> {
-        if (!data.id) throw new Error('eventId must be provided');
+    async getRegistration(data: IGetRegistrationRequest): Promise<IGetRegistrationResponse> {
+        const { userId, eventId } = data;
 
-        const event = await this.repository.findById(data.id);
-        if (!event) throw new Error('Event not found');
+        if (!userId || !eventId) {
+            throw new Error('userId and eventId must be provided');
+        }
+
+        const registration = await this.repository.findRegistration(eventId, userId);
+        if (!registration) {
+            throw new Error('Registration not found');
+        }
+
+        if (registration.status !== RegistrationStatus.WAITING_PAYMENT) {
+            throw new Error('Registration is not in WAITING_PAYMENT status');
+        }
+
+        const event = await this.repository.findById(eventId);
+        if (!event) {
+            throw new Error('Event not found');
+        }
+
+        const user = await this.repository.findUserById(userId);
+        if (!user) {
+            throw new Error('User not found');
+        }
 
         const totalRegistrations = await this.repository.countRegistrations(event.id);
         const hasVacancy = totalRegistrations < event.capacity;
 
         return {
-        id: event.id,
-        title: event.title,
-        description: event.description ?? '',
-        startAt: event.start_at.toISOString(),
-        endAt: event.end_at.toISOString(),
-        price: event.price ?? 0,
-        saleStartAt: event.sale_start_at?.toISOString() ?? '',
-        saleEndAt: event.sale_end_at?.toISOString() ?? '',
-        addressStreet: event.address_street,
-        addressNumber: event.address_number ?? '',
-        addressCity: event.address_city,
-        addressState: event.address_state,
-        addressZipcode: event.address_zipcode,
-        addressCountry: event.address_country,
-        capacity: event.capacity,
-        isFree: event.is_free,
-        eventType: event.event_type,
-        status: event.status,
-        createdAt: event.created_at.toISOString(),
-        updatedAt: event.updated_at.toISOString(),
-        hasVacancy,
+            id: event.id,
+            title: event.title,
+            description: event.description ?? '',
+            startAt: event.start_at.toISOString(),
+            endAt: event.end_at.toISOString(),
+            price: event.price ?? 0,
+            saleStartAt: event.sale_start_at?.toISOString() ?? '',
+            saleEndAt: event.sale_end_at?.toISOString() ?? '',
+            addressStreet: event.address_street,
+            addressNumber: event.address_number ?? '',
+            addressCity: event.address_city,
+            addressState: event.address_state,
+            addressZipcode: event.address_zipcode,
+            addressCountry: event.address_country,
+            capacity: event.capacity,
+            isFree: event.is_free,
+            eventType: event.event_type,
+            status: event.status,
+            createdAt: event.created_at.toISOString(),
+            updatedAt: event.updated_at.toISOString(),
+            hasVacancy,
+            cpf: user.cpf,
         };
     }
 
