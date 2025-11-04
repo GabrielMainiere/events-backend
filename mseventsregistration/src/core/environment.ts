d248f5af3ae322527348a1be2ada05c5841b2d0b
Luiz Reis <reisluiz05@gmail.com>
2025-11-04 12:53:39 -0300

    comunicating mseventsregistration and msusers via docker

--- mseventsregistration/src/core/environment.ts
+++ mseventsregistration/src/core/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   databaseUrl: process.env.DATABASE_URL || 'postgresql://user:pass@localhost:5432/dbname',
   graphqlPort: Number(process.env.GRAPHQL_PORT ?? '3001'),
   grpcPort: Number(process.env.GRPC_PORT ?? '50051'),
-  grpcPortMsusers: Number(process.env.GRPC_PORT_MSUSERS ?? '50053'),
+  grpcUsers: process.env.GRPC_USERS || 'http://localhost:50051',
   grpcPaymentsPort: Number(process.env.GRPC_PORT_MSPAYMENT ?? '50054'),
 };
