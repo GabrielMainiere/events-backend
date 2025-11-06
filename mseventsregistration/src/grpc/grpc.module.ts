dfae2980f29028e047e7ad340fbc0d64d2eb1168
Rafael Belmont <160741467+BelmontRafael@users.noreply.github.com>
2025-11-06 01:09:57 -0300

    implementing notification when an user is registered at some event (#50)

--- mseventsregistration/src/grpc/grpc.module.ts
+++ mseventsregistration/src/grpc/grpc.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EventRegistrationModule } from './event-registration/eventsRegistration.module';
 import { UsersModule } from './users/users.module';
 import { EventRegistrationPaymentsModule } from './event-registration/eventRegistrationPayment.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [EventRegistrationModule, UsersModule, EventRegistrationPaymentsModule],
-  exports: [UsersModule],
+  imports: [EventRegistrationModule, UsersModule, EventRegistrationPaymentsModule, NotificationsModule],
+  exports: [UsersModule, NotificationsModule],
 })
 export class GrpcModule {}
