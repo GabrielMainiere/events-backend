13df69279fd70473c9a47691bb5ac77b2eafb597
Luiz Reis <reisluiz05@gmail.com>
2025-11-07 21:17:27 -0300

    project created

---
+++ mscurrency/src/main/java/br/com/mscurrency/MscurrencyApplication.java
@@ -0,0 +1,13 @@
+package br.com.mscurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscurrencyApplication.class, args);
+    }
+
+}
---
+++ mscurrency/src/test/java/br/com/mscurrency/MscurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mscurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
