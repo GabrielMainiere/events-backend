21afec9d48468c6ead99844b81917864028f0512
Lucas Castelano <100870230+Lucas-Cast@users.noreply.github.com>
2025-11-08 09:48:15 -0300

    [MSEVENTS] feat: msevents routes auth (#63)

--- msevents/src/app.module.ts
+++ msevents/src/app.module.ts
@@ -3,14 +3,18 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { EventsModule } from './events/events.module';
 import { join } from 'path';
+import { authGuardProvider } from './auth/auth.provider';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     EventsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
     }),
+    AuthModule
   ],
+  providers: [authGuardProvider]
 })
 export class AppModule {}
--- msevents/src/core/environment.ts
+++ msevents/src/core/environment.ts
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 
 export const environment = {
-  databaseUrl: process.env.DATABASE_URL || 'postgresql://user:pass@localhost:5432/dbname',
+  databaseUrl:
+    process.env.DATABASE_URL || 'postgresql://user:pass@localhost:5432/dbname',
   graphqlPort: Number(process.env.GRAPHQL_PORT ?? '3000'),
-  grpcMseventsregistrations: process.env.GRPC_MSEVENTSREGISTRATION ?? 'http://localhost:50051',
+  grpcMseventsregistrations:
+    process.env.GRPC_MSEVENTSREGISTRATION ?? 'http://localhost:50051',
+  jwtPass:
+    process.env.JWT_PASS || 'DHSAGFBASHDGBASNDMKASDHJIOSAHDOJISADHASJOKIL'
 };
