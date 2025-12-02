c37b17678a804a8f5791e3094d88549b91509e30
Luiz Reis <reisluiz05@gmail.com>
2025-12-01 21:38:22 -0300

    creating new project

---
+++ mspayments-refactor/src/test/java/br/com/mspaymentsrefactor/MspaymentsRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mspaymentsrefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspaymentsRefactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
