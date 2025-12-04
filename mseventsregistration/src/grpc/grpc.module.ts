a08bd628b38621c2a5708e71d405ce5cde39f21f
Lucas Castelano <lucas.castelano27@gmail.com>
2025-12-04 08:20:34 -0300

    [MS-EVENT-REGISTRATION]refactor: use modules folder structure

--- mseventsregistration/src/grpc/grpc.module.ts
+++ mseventsregistration/src/grpc/grpc.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EventRegistrationModule } from './event-registration/eventsRegistration.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from '../modules/users/users.module';
 import { EventRegistrationPaymentsModule } from './event-registration/eventRegistrationPayment.module';
 
 @Module({
-  imports: [EventRegistrationModule, UsersModule, EventRegistrationPaymentsModule],
-  exports: [UsersModule],
+  imports: [
+    EventRegistrationModule,
+    UsersModule,
+    EventRegistrationPaymentsModule
+  ],
+  exports: [UsersModule]
 })
 export class GrpcModule {}
