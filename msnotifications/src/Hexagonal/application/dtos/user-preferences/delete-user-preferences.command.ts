5cafb450f2b1928a4ac42637dc87a651c42766f9
BelmontRafael <rafaelmagalhaes12323@gmail.com>
2025-12-05 23:16:09 -0300

    implementing get user preference use case

--- msnotifications/src/Hexagonal/application/dtos/user-preferences/delete-user-preferences.command.ts
+++
@@ -1,8 +0,0 @@
-import { IsUUID, IsNotEmpty } from 'class-validator';
-
-export class DeleteUserPreferencesCommand {
-  
-  @IsUUID(4, { message: 'User ID must be a valid UUID v4' })
-  @IsNotEmpty({ message: 'User ID is required' })
-  userId: string;
-}
--- msnotifications/src/Hexagonal/application/ports/input/user-preferences/delete-user-preferences.port.ts
+++
@@ -1,5 +0,0 @@
-import { DeleteUserPreferencesCommand } from "src/Hexagonal/application/dtos/user-preferences/delete-user-preferences.command";
-
-export interface IDeleteUserPreferences {
-  execute(command: DeleteUserPreferencesCommand): Promise<void>;
-}
