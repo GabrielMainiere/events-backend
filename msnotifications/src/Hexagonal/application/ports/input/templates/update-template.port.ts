9333cb45c071c39d7a8d32110b098108be5772cc
BelmontRafael <rafaelmagalhaes12323@gmail.com>
2025-12-05 23:00:28 -0300

    implementing update-template use-case.ts

--- msnotifications/src/Hexagonal/application/ports/input/templates/update-template.port.ts
+++ msnotifications/src/Hexagonal/application/ports/input/templates/update-template.port.ts
@@ -1,5 +1,6 @@
+import { TemplateResponse } from "src/Hexagonal/application/dtos/templates/template.response";
 import { UpdateTemplateCommand } from "src/Hexagonal/application/dtos/templates/update-template.command";
 
 export interface IUpdateTemplate {
-  execute(command: UpdateTemplateCommand): Promise<void>;
+  execute(command: UpdateTemplateCommand): Promise<TemplateResponse>;
 }
